67f33541d9886d85579c64dece42e5b1ac938fa5
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-06-14 01:53:18 -0700

    plus: use + postfix

--- plus.sh
+++ plus.sh
@@ -3,7 +3,7 @@
 #export CCACHE_DIR=/home/mustaavalkosta/storage/ccache-3.1.9
 export USER=mustaavalkosta
 
-RELEASE_NAME="BR4"
+RELEASE_NAME="BR4+"
 
 SOURCE_ROOT=/home/mustaavalkosta/storage/cm11.0_stable
 
