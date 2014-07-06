08f5cf4be79aa03a6d3a5ab029ee43157c85f65b
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-07-06 03:29:24 -0700

    kill odexed builds for good

--- nightly.sh
+++ nightly.sh
@@ -35,32 +35,5 @@ fi
 
 rsync -avvruO -e ssh --delete --exclude '*.md5sum' --exclude '.cm-11-*' $DOWNLOAD_DIR Mustaavalkosta@upload.goo.im:$REMOTE_DIR
 
-
-## Odexed nightly ##
-#DOWNLOAD_DIR=/home/mustaavalkosta/downloads/cm-11-unofficial-ace/nightlies-odexed/
-#REMOTE_DIR=/home/Mustaavalkosta/public_html/cm-11-unofficial-ace/nightlies-odexed/
-#
-#cd $SOURCE_ROOT
-#make clean
-#source build/envsetup.sh
-#lunch cm_ace-userdebug
-#TARGET_WITH_DEXPREOPT=true mka bacon
-#
-#if [ $? -eq 0 ]
-#then
-#    cp -v $SOURCE_ROOT/out/target/product/ace/cm-11-*-UNOFFICIAL-ODEXED-ace.zip* $DOWNLOAD_DIR
-#    make clean
-#    cd $DOWNLOAD_DIR
-#    rm -v `ls -t $DOWNLOAD_DIR | awk 'NR>24'`
-#    cd $SOURCE_ROOT
-#else
-#    echo "##############################################################"
-#    echo "##                        BUILD FAILED                      ##"
-#    echo "##############################################################"
-#    exit 0
-#fi
-#
-#rsync -avvru -e ssh --delete --exclude '*.md5sum' $DOWNLOAD_DIR Mustaavalkosta@upload.goo.im:$REMOTE_DIR
-
 ## Sync extras also
 rsync -avvruO -e ssh --delete --exclude '*.md5sum' /home/mustaavalkosta/downloads/cm-11-unofficial-ace/extras/ Mustaavalkosta@upload.goo.im:/home/Mustaavalkosta/public_html/cm-11-unofficial-ace/extras/
--- release.sh
+++ release.sh
@@ -35,30 +35,5 @@ fi
 
 rsync -avvruO -e ssh --delete --exclude '*.md5sum' --exclude '.cm-11-*' $DOWNLOAD_DIR Mustaavalkosta@upload.goo.im:$REMOTE_DIR
 
-
-## Odexed release ##
-#DOWNLOAD_DIR=/home/mustaavalkosta/downloads/cm-11-unofficial-ace/releases-odexed/
-#REMOTE_DIR=/home/Mustaavalkosta/public_html/cm-11-unofficial-ace/releases-odexed/
-#
-#cd $SOURCE_ROOT
-#make clean
-#source build/envsetup.sh
-#lunch cm_ace-userdebug
-#TARGET_WITH_DEXPREOPT=true TARGET_UNOFFICIAL_BUILD_ID="$RELEASE_NAME" mka bacon
-#
-#if [ $? -eq 0 ]
-#then
-#    cp -v $SOURCE_ROOT/out/target/product/ace/cm-11-*-UNOFFICIAL-ODEXED-$RELEASE_NAME-ace.zip* $DOWNLOAD_DIR
-#    make clean
-#    cd $SOURCE_ROOT
-#else
-#    echo "##############################################################"
-#    echo "##                        BUILD FAILED                      ##"
-#    echo "##############################################################"
-#    exit 0
-#fi
-#
-#rsync -avvru -e ssh --delete --exclude '*.md5sum' $DOWNLOAD_DIR Mustaavalkosta@upload.goo.im:$REMOTE_DIR
-
 ## Sync extras also
 rsync -avvruO -e ssh --delete --exclude '*.md5sum' /home/mustaavalkosta/downloads/cm-11-unofficial-ace/extras/ Mustaavalkosta@upload.goo.im:/home/Mustaavalkosta/public_html/cm-11-unofficial-ace/extras/
