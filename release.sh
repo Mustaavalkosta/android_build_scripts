ede111f2a97c6356c746e2b71ef28d6dc9028b3f
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-07-08 04:55:15 -0700

    M8 and M8Plus

--- release.sh
+++ release.sh
@@ -3,7 +3,7 @@
 #export CCACHE_DIR=/home/mustaavalkosta/storage/ccache-3.1.9
 export USER=mustaavalkosta
 
-RELEASE_NAME="BR4"
+RELEASE_NAME="M8"
 
 SOURCE_ROOT=/home/mustaavalkosta/storage/cm_release
 
