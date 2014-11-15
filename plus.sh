2c123ca0574088589378275f1f04045970302913
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2014-11-15 02:44:48 -0800

    update release and plus

--- plus.sh
+++ plus.sh
@@ -14,42 +14,63 @@ RELEASE_NAME="M11Plus"
 # Android source tree root
 SOURCE_ROOT=/home/mustaavalkosta/storage/cm_release
 
-# Local dirs on codefi.re server
-LOCAL_DIR=/home/mustaavalkosta/downloads/cm-11-unofficial-ace/plus/
-LOCAL_EXTRAS_DIR=/home/mustaavalkosta/downloads/cm-11-unofficial-ace/extras/
+build()
+{
+    # Device
+    local DEVICE=$1
 
-# Remote dirs on goo.im server
-REMOTE_DIR=/home/Mustaavalkosta/public_html/cm-11-unofficial-ace/plus/
-REMOTE_EXTRAS_DIR=/home/Mustaavalkosta/public_html/cm-11-unofficial-ace/extras/
+    # Local dirs on codefi.re server
+    local LOCAL_DIR=/home/mustaavalkosta/downloads/cm-11-unofficial-$DEVICE/plus/
+    if [ "$DEVICE" = "ace" ]
+    then
+        local LOCAL_EXTRAS_DIR=/home/mustaavalkosta/downloads/cm-11-unofficial-$DEVICE/extras/
+    fi
 
-# Run build
-cd $SOURCE_ROOT
-make clean
-repo sync local_manifest # update manifest to bring in manifest changes first
-repo sync -j8
-cd $SOURCE_ROOT/vendor/cm
-./get-prebuilts
-cd $SOURCE_ROOT
-source build/envsetup.sh
-lunch cm_ace-userdebug
-TARGET_UNOFFICIAL_BUILD_ID="$RELEASE_NAME" mka bacon
+    # Remote dirs on goo.im server
+    local REMOTE_DIR=/home/Mustaavalkosta/public_html/cm-11-unofficial-$DEVICE/plus/
+    if [ "$DEVICE" = "ace" ]
+    then
+        local REMOTE_EXTRAS_DIR=/home/Mustaavalkosta/public_html/cm-11-unofficial-$DEVICE/extras/
+    fi
 
-# Check for build fail
-if [ $? -eq 0 ]
-then
-    cp -v $SOURCE_ROOT/out/target/product/ace/cm-11-*-UNOFFICIAL-$RELEASE_NAME-ace.zip* $LOCAL_DIR
-    make clean
+    # Run build
     cd $SOURCE_ROOT
-else
-    echo "##############################################################"
-    echo "##                        BUILD FAILED                      ##"
-    echo "##############################################################"
-    exit 0
-fi
+    make clean
+    repo sync local_manifest # update manifest to bring in manifest changes first
+    repo sync -j8
+    cd $SOURCE_ROOT/vendor/cm
+    ./get-prebuilts
+    cd $SOURCE_ROOT
+    source build/envsetup.sh
+    lunch cm_$DEVICE-userdebug
+    TARGET_UNOFFICIAL_BUILD_ID="$RELEASE_NAME" mka bacon
 
-# Basketbuild
-bash /home/mustaavalkosta/storage/build_scripts/basketbuild.sh
+    # Check for build fail
+    if [ $? -eq 0 ]
+    then
+        cp -v $SOURCE_ROOT/out/target/product/ace/cm-11-*-UNOFFICIAL-$RELEASE_NAME-ace.zip* $LOCAL_DIR
+        make clean
+        cd $SOURCE_ROOT
+    else
+        echo "##############################################################"
+        echo "##                        BUILD FAILED                      ##"
+        echo "##############################################################"
+        exit 0
+    fi
 
-# Sync with goo.im
-rsync -avvruO -e ssh --delete --timeout=60 --exclude '*.md5sum' --exclude '.cm-11-*' $LOCAL_DIR Mustaavalkosta@upload.goo.im:$REMOTE_DIR
-rsync -avvruO -e ssh --delete --timeout=60 --exclude '*.md5sum' $LOCAL_EXTRAS_DIR Mustaavalkosta@upload.goo.im:$REMOTE_EXTRAS_DIR
+    # Basketbuild
+    bash /home/mustaavalkosta/storage/build_scripts/basketbuild.sh $DEVICE
+
+    # Sync with goo.im
+    rsync -avvruO -e ssh --delete --timeout=60 --exclude '*.md5sum' --exclude '.cm-11-*' $LOCAL_DIR Mustaavalkosta@upload.goo.im:$REMOTE_DIR
+    if [ "$DEVICE" = "ace" ]
+    then
+        rsync -avvruO -e ssh --delete --timeout=60 --exclude '*.md5sum' $LOCAL_EXTRAS_DIR Mustaavalkosta@upload.goo.im:$REMOTE_EXTRAS_DIR
+    fi
+}
+
+# Build ace
+build "ace"
+
+# Build saga
+#build "saga"
