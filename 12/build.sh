09a227fce9659dafc02ef8d7a4dd542cb9097ede
Mustaavalkosta <niko.hyrynsalmi@gmail.com>
2015-01-02 01:32:50 -0800

    add build.sh for CM12.0

---
+++ 12/build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# Build script for CM 12.0
+
+if [ -z "$1" ]
+then
+    echo "Insufficient parameters. Usage: `basename $0` [nightly|snapshot]"
+    exit 0
+fi
+
+# Base dir for this script mess
+SCRIPT_DIR="/home/mustaavalkosta/storage/build_scripts"
+
+# Cron doesn't get this without exporting it here.
+export USER=mustaavalkosta
+
+# CM version
+CM_VERSION=12
+
+# Release name for release build
+RELEASE_NAME="M1"
+
+# Base path for downloads
+LOCAL_BASE_DIR=/home/mustaavalkosta/downloads
+
+# Basketbuild login information
+# File format:
+# USERNAME:PASSWORD
+BASKET_PASSWD_FILE="$SCRIPT_DIR/.basket_passwd"
+
+source "$SCRIPT_DIR/include/basketbuild.sh"
+
+if [ "$1" == "nightly" ]
+then
+    source "$SCRIPT_DIR/include/nightly.sh"
+    build "ace"
+    build "saga"
+elif [ "$1" == "snapshot" ]
+then
+    source "$SCRIPT_DIR/include/snapshot.sh"
+    build "ace"
+    #build "saga"
+else
+    echo "Invalid release type"
+    exit 0
+fi
+
